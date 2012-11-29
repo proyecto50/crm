c9bdf01c8da916ac46cc9241963043ab0607606c
jhon <jhon@JHON>
2012-11-29 15:38:47 -0500

    0.0

--- base.php
+++ base.php
@@ -9,18 +9,6 @@
 	 * @property CI_Table $table
 	 * @property CI_Session $session
 	 * @property CI_FTP $ftp         
-         * @property clientes_model $clientes_model
-         * @property ruteros_model $ruteros_model
-         * @property notas_model $notas_model
-         * @property cargue_model $cargue_model
-         * @property insumos_model $insumos_model
-         * @property productos_model $productos_model
-         * @property syncup_model $syncup_model
-         * @property iventas_model $iventas_model
-         * @property ntransp_model $ntransp_model
-         * @property prestamos_model $prestamos_model
-         * @property desempeno_model $desempeno_model
-         * @property inventario_model $inventario_model
          * 
      *
      */
